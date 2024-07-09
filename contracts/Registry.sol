24986a56b2a60fa8e45df834fb9e990880433e28
Benjamin Oh <benjioh5@gmail.com>
2024-07-09 20:27:54 +0900

    REG-01 | Usage of `transfer()` for sending Ether

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -229,14 +229,38 @@ contract Registry is Owned, IMetadataRegistry, IOwnerRegistry, IReverseRegistry 
         return true;
     }
 
+    /**
+     * @dev Replacement for Solidity's `transfer`: sends `amount` wei to
+     * `recipient`, forwarding all available gas and reverting on errors.
+     *
+     * https://eips.ethereum.org/EIPS/eip-1884[EIP1884] increases the gas cost
+     * of certain opcodes, possibly making contracts go over the 2300 gas limit
+     * imposed by `transfer`, making them unable to receive funds via
+     * `transfer`. {sendValue} removes this limitation.
+     *
+     * https://diligence.consensys.net/posts/2019/09/stop-using-soliditys-transfer-now/[Learn more].
+     *
+     * IMPORTANT: because control is transferred to `recipient`, care must be
+     * taken to not create reentrancy vulnerabilities. Consider using
+     * {ReentrancyGuard} or the
+     * https://solidity.readthedocs.io/en/v0.5.11/security-considerations.html#use-the-checks-effects-interactions-pattern[checks-effects-interactions pattern].
+     */
+    function sendValue(address payable recipient, uint256 amount) internal {
+        require(address(this).balance >= amount, "Address: insufficient balance");
+
+        // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
+        (bool success, ) = recipient.call{ value: amount }("");
+        require(success, "Address: unable to send value, recipient may have reverted");
+    }
+
     function drain()
         external
         onlyOwner
         returns (bool)
     {
         emit Drained(address(this).balance);
-        msg.sender.transfer(address(this).balance);
-        return true;
+        //msg.sender.transfer(address(this).balance);
+        return sendValue(msg.sender, address(this).balance);
     }
 
     // MetadataRegistry views
