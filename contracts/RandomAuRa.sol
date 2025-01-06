3c608fb66e0a49c7833f6a335357862a6cc9c57d
tim <sungmin.kim@kondor.co.kr>
2025-01-06 13:23:35 +0900

    chor: Add detailed comments for improved clarity and maintainability of RandomAuRa contract

--- contracts/RandomAuRa.sol
+++ contracts/RandomAuRa.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.5.10;
+// Import necessary interfaces and functionality for upgradeable contracts.
 
 import "./interfaces/IRandomAuRa.sol";
 import "./interfaces/IStakingAuRa.sol";
