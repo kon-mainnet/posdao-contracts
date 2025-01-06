a322f3aa5fd99458ead9d985dd571a18b200fc00
tim <sungmin.kim@kondor.co.kr>
2025-01-06 15:01:16 +0900

    feat: Implement renounceAdmin function to enable admin rights relinquishment
    - Added `renounceAdmin` function to allow the current admin to renounce administrative rights.
    - Ensures the contract operates without administrative oversight post-configuration.
    - Includes safety checks to confirm the caller is the current admin.
    - Once invoked, the action is irreversible; the admin role is permanently removed.

--- contracts/upgradeability/AdminUpgradeabilityProxy.sol
+++ contracts/upgradeability/AdminUpgradeabilityProxy.sol
@@ -5,7 +5,7 @@ import "./BaseAdminUpgradeabilityProxy.sol";
 
 /**
  * @title AdminUpgradeabilityProxy
- * @dev Extends from BaseAdminUpgradeabilityProxy with a constructor for 
+ * @dev Extends from BaseAdminUpgradeabilityProxy with a constructor for
  * initializing the implementation, admin, and init data.
  */
 contract AdminUpgradeabilityProxy is BaseAdminUpgradeabilityProxy, UpgradeabilityProxy {
@@ -18,12 +18,22 @@ contract AdminUpgradeabilityProxy is BaseAdminUpgradeabilityProxy, Upgradeabilit
         assert(ADMIN_SLOT == bytes32(uint256(keccak256('eip1967.proxy.admin')) - 1));
         _setAdmin(_admin);
     }
-
+    /**
+     * @dev This function should be invoked once the contract has been fully configured
+     * and the admin role is no longer required for its operation.
+     * Calling this function effectively renounces the admin rights,
+     * ensuring the contract operates in a decentralized manner without administrative oversight.
+     * Note: This action is irreversible; proceed with caution.
+     */
+    function renounceAdmin()  {
+        require(msg.sender == _admin(), "Cannot call fallback function from the proxy admin");
+        _setAdmin(address(0));
+    }
     /**
      * @dev Only fall back when the sender is not the admin.
      */
     function _willFallback() internal {
-    require(msg.sender != _admin(), "Cannot call fallback function from the proxy admin");
-    super._willFallback();
+        require(msg.sender != _admin(), "Cannot call fallback function from the proxy admin");
+        super._willFallback();
     }
 }
